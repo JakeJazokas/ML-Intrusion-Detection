9da1e744bbc7b69162e11da4d0b9632cc0f7e5b1
Jake <jakejazokas@outlook.com>
2022-03-27 13:52:21 -0400

    Added ACK frames and outline for the model

--- FrameToFlow.py
+++ FrameToFlow.py
@@ -1,15 +1,26 @@
 import pyshark
 
-def capture_to_n_gram_mapping(captrue_path):
+def extract_feature_set_from_capture(captrue_path):
     captrue_array = pyshark.FileCapture(captrue_path)
-    n_gram_map = dict()
+    feature_set = dict()
     # print(dir(captrue_array[1]['wlan']))
     # print(captrue_array[1]['wlan']._all_fields)
     for capture in captrue_array:
+        # print(capture)
         capture_fields = capture.wlan._all_fields
         if(capture_fields['wlan.fc.type_subtype'] == '29'):
-            # No src address for an ACK frame
-            continue
+            # No src address for an ACK frame only dst
+            # We want to include this in the n-gram
+            feature_array = []
+            # Source = none
+            feature_array.append(None)
+            # Recieve = dst address
+            feature_array.append(capture_fields['wlan.ra'])
+            # Frame type
+            feature_array.append(capture_fields['wlan.fc.type'])
+            # Frame subtype
+            feature_array.append(capture_fields['wlan.fc.subtype'])
+            feature_set[capture.frame_info.time_epoch] = feature_array
         elif(capture_fields['wlan.fc.type_subtype'] == '33'):
             # Malformed packet, retransmission nessicary
             continue
@@ -34,10 +45,11 @@ def capture_to_n_gram_mapping(captrue_path):
             feature_array.append(capture_fields['wlan.fc.type'])
             # Frame subtype
             feature_array.append(capture_fields['wlan.fc.subtype'])
-            n_gram_map[capture.frame_info.time_epoch] = feature_array
-            # TODO Hash the type and subtype togeather, and use t (time) to create n-gram flows of frames
+            feature_set[capture.frame_info.time_epoch] = feature_array
+            # TODO Hash the type and subtype togeather
+            # TODO use t (time) to create n-gram flows of frames (Auth, Asso, Data, Deauth)
     captrue_array.close()
-    # print(n_gram_map)
-    return(n_gram_map)
+    print(feature_set)
+    return(feature_set)
 
-capture_to_n_gram_mapping('Project/Wireshark_802_11.pcap')
+extract_feature_set_from_capture('Wireshark_802_11.pcap')
---
+++ WIDSModel.py
@@ -0,0 +1,8 @@
+
+# Features used to build the model
+# 1) Flow Probability - Jelinek-Mercer smoothing model - (Lambda = 0.5)
+# 2) Total Frames
+# 3) New n-grams in flow (Difference from last input)
+# 4) Ratio of number of management frames to total frames
+# 5) Ratio of number of control frames to total frames
+# 6) Ratio of number of data frames to total frames
