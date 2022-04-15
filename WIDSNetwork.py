2de8b3831f0e7583409e7ed668ee865ffc401f32
Jake <jakejazokas@outlook.com>
2022-04-14 21:46:02 -0400

    generate new plot types

--- WIDSNetwork.py
+++ WIDSNetwork.py
@@ -1,3 +1,4 @@
+import matplotlib
 from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
 from CaptureToFlow import CaptureToFlow
@@ -239,9 +240,16 @@ def generate_false_positives(predicted, actual):
             fp_counter += 1
     return(fp_counter/len(predicted))
 
+def generate_true_positives(predicted, actual):
+    tp_counter = 0
+    for idx, value in enumerate(predicted):
+        if(value == 1 and actual[idx] == 1):
+            tp_counter += 1
+    return(tp_counter/len(predicted))
+
 def generate_custom_attack_csv_files():
     get_custom_data_from_dataset("datasets\(Re)Assoc_29.csv", None, "datasets\output\AssocAttack.csv", False)
-    # get_custom_data_from_dataset("datasets\Botnet_18.csv", None, "datasets\output\BotnetAttack.csv", False)
+    get_custom_data_from_dataset("datasets\Botnet_18.csv", None, "datasets\output\BotnetAttack.csv", False)
     get_custom_data_from_dataset("datasets\Deauth_22.csv", None, "datasets\output\DeauthAttack.csv", False)
     get_custom_data_from_dataset("datasets\Disas_38.csv", None, "datasets\output\DisasAttack.csv", False)
     get_custom_data_from_dataset("datasets\Evil_Twin_28.csv", None, "datasets\output\EvilTwinAttack.csv", False)
@@ -252,59 +260,72 @@ def generate_custom_attack_csv_files():
     get_custom_data_from_dataset("datasets\Malware_0.csv", None, "datasets\output\MalwareAttack.csv", False)
     get_custom_data_from_dataset("datasets\RogueAP_0.csv", None, "datasets\output\RougeAPAttack.csv", False)
     get_custom_data_from_dataset("datasets\SQL_Injection_0.csv", None, "datasets\output\SQLAttack.csv", False)
-    # get_custom_data_from_dataset("datasets\SSDP_18.csv", None, "datasets\output\SSDPAttack.csv", False)
+    get_custom_data_from_dataset("datasets\SSDP_18.csv", None, "datasets\output\SSDPAttack.csv", False)
     get_custom_data_from_dataset("datasets\SSH_0.csv", None, "datasets\output\SSHAttack.csv", False)
     get_custom_data_from_dataset("datasets\Website_spoofing_0.csv", None, "datasets\output\WebsiteSpoofAttack.csv", False)
 
 def generate_attack_prediction_accuracy(model, attack_dataset):
     x, y = get_n_grams_from_custom_dataset(attack_dataset)
     predicted_values = used_trained_model_to_predit_flow(model, np.asarray(x))
-    return generate_test_accuracy(predicted_values, y), generate_false_positives(predicted_values, y)
+    return generate_test_accuracy(predicted_values, y), generate_false_positives(predicted_values, y), generate_true_positives(predicted_values, y)
 
 def generate_predictions_for_all_attacks(model):
-    p1,fp1 = generate_attack_prediction_accuracy(model, "datasets\output\AssocAttack.csv")
-    # p2 = generate_attack_prediction_accuracy(model, "datasets\output\BotnetAttack.csv")
-    p3,fp3 = generate_attack_prediction_accuracy(model, "datasets\output\DeauthAttack.csv")
-    p4,fp4 = generate_attack_prediction_accuracy(model, "datasets\output\DisasAttack.csv")
-    p5,fp5 = generate_attack_prediction_accuracy(model, "datasets\output\EvilTwinAttack.csv")
-    p6,fp6 = generate_attack_prediction_accuracy(model, "datasets\output\Kr00kAttack.csv")
-    p7,fp7 = generate_attack_prediction_accuracy(model, "datasets\output\KrackAttack.csv")
-    p8,fp8 = generate_attack_prediction_accuracy(model, "datasets\output\MalwareAttack.csv")
-    p9,fp9 = generate_attack_prediction_accuracy(model, "datasets\output\RougeAPAttack.csv")
-    p10,fp10 = generate_attack_prediction_accuracy(model, "datasets\output\SQLAttack.csv")
-    # p11 = generate_attack_prediction_accuracy(model, "datasets\output\SSDPAttack.csv")
-    p12,fp12 = generate_attack_prediction_accuracy(model, "datasets\output\SSHAttack.csv")
-    p13,fp13 = generate_attack_prediction_accuracy(model, "datasets\output\WebsiteSpoofAttack.csv")
+    p1,fp1,tp1 = generate_attack_prediction_accuracy(model, "datasets\output\AssocAttack.csv")
+    p2,fp2,tp2 = generate_attack_prediction_accuracy(model, "datasets\output\BotnetAttack.csv")
+    p3,fp3,tp3 = generate_attack_prediction_accuracy(model, "datasets\output\DeauthAttack.csv")
+    p4,fp4,tp4 = generate_attack_prediction_accuracy(model, "datasets\output\DisasAttack.csv")
+    p5,fp5,tp5 = generate_attack_prediction_accuracy(model, "datasets\output\EvilTwinAttack.csv")
+    p6,fp6,tp6 = generate_attack_prediction_accuracy(model, "datasets\output\Kr00kAttack.csv")
+    p7,fp7,tp7 = generate_attack_prediction_accuracy(model, "datasets\output\KrackAttack.csv")
+    p8,fp8,tp8 = generate_attack_prediction_accuracy(model, "datasets\output\MalwareAttack.csv")
+    p9,fp9,tp9 = generate_attack_prediction_accuracy(model, "datasets\output\RougeAPAttack.csv")
+    p10,fp10,tp10 = generate_attack_prediction_accuracy(model, "datasets\output\SQLAttack.csv")
+    p11,fp11,tp11 = generate_attack_prediction_accuracy(model, "datasets\output\SSDPAttack.csv")
+    p12,fp12,tp12 = generate_attack_prediction_accuracy(model, "datasets\output\SSHAttack.csv")
+    p13,fp13,tp13 = generate_attack_prediction_accuracy(model, "datasets\output\WebsiteSpoofAttack.csv")
     return (
-        [p1*100,p3*100,p4*100,p5*100,p6*100,p7*100,p8*100,p9*100,p10*100,p12*100,p13*100], 
-        [fp1*100,fp3*100,fp4*100,fp5*100,fp6*100,fp7*100,fp8*100,fp9*100,fp10*100,fp12*100,fp13*100] 
+        [p1*100,p2*100,p3*100,p4*100,p5*100,p6*100,p7*100,p8*100,p9*100,p10*100,p11*100,p12*100,p13*100], 
+        [fp1*100,fp2*100,fp3*100,fp4*100,fp5*100,fp6*100,fp7*100,fp8*100,fp9*100,fp11*100,fp10*100,fp12*100,fp13*100],
+        [tp1*100,tp2*100,tp3*100,tp4*100,tp5*100,tp6*100,tp7*100,tp8*100,tp9*100,tp11*100,tp10*100,tp12*100,tp13*100]
     )
-    # return[p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13]
 
-def generate_prediction_graphs(pred_accuracy_array, false_positive_array):
+def generate_prediction_graphs(pred_accuracy_array, false_positive_array, true_positive_array):
     attack_names = [
-        'Assoc','Deauth','Disas','EvilTwin',
-        'Kr00k','Krack','Malware','RougeAP','SQL',
+        'Re-Assoc','Botnet','Deauth','DisAssoc','EvilTwin',
+        'Kr00k','Krack','Malware','RougeAP','SQL','SSDP',
         'SSH','WebsiteSpoof'
     ]
-    # attack_names = [
-    #     'Assoc','Botnet','Deauth','Disas','EvilTwin',
-    #     'Kr00k','Krack','Malware','RougeAP','SQL',
-    #     'SSDP', 'SSH','WebsiteSpoof'
-    # ]
-    fig, ax = plt.subplots(2, figsize=(13,9))
-    ax[0].set_axisbelow(True)
-    ax[0].grid(color='gray', linestyle='dashed')
-    ax[1].set_axisbelow(True)
-    ax[1].grid(color='gray', linestyle='dashed')
-    ax[0].set_title('Test accuracy % per attack')
-    ax[0].bar(attack_names, pred_accuracy_array, color=[(0, (191-(10*x))/255, 1) for x in range(12)], edgecolor='black')
-    ax[1].set_title('False Positive % per attack')
-    ax[1].bar(attack_names, false_positive_array, color=[((255-(10*x))/255, 3/255, 27/255) for x in range(12)], edgecolor='black')
-    ax[0].set_ylim([0, 100])
-    ax[1].set_ylim([0, 100])
-    ax[0].set_yticks(np.arange(0,101,10))
-    ax[1].set_yticks(np.arange(0,101,10))
+    common_names = ['Deauth', 'DisAssoc','EvilTwin']
+    false_positive_array = false_positive_array[2:5]
+    true_positive_array = true_positive_array[2:5]
+    # Plots
+    fig = plt.figure()
+    fig.set_figwidth(13)
+    gs = fig.add_gridspec(2,2)
+    ax1 = fig.add_subplot(gs[0, 0])
+    ax2 = fig.add_subplot(gs[0, 1])
+    ax0 = fig.add_subplot(gs[1, :])
+    # Set grids
+    ax0.set_axisbelow(True)
+    ax0.grid(color='gray', linestyle='dashed')
+    ax1.set_axisbelow(True)
+    ax1.grid(color='gray', linestyle='dashed')
+    ax2.set_axisbelow(True)
+    ax2.grid(color='gray', linestyle='dashed')
+    # Set titles and values
+    ax0.set_title('Test accuracy % per attack in AWID3 attack database')
+    ax0.bar(attack_names, pred_accuracy_array, color=[(0, (191-(10*x))/255, 1) for x in range(14)], edgecolor='black')
+    ax1.set_title('False Positive % per common attack with paper')
+    ax1.bar(common_names, false_positive_array, color=[((255-(20*x))/255, 3/255, 27/255) for x in range(3)], edgecolor='black')
+    ax2.set_title('True Positive % per common attack with paper')
+    ax2.bar(common_names, true_positive_array, color=[(0, (191-(20*x))/255, 0) for x in range(3)], edgecolor='black')
+    # Axis values
+    ax0.set_ylim([0, 100])
+    ax1.set_ylim([0, 100])
+    ax2.set_ylim([0, 100])
+    ax0.set_yticks(np.arange(0,101,10))
+    ax1.set_yticks(np.arange(0,101,10))
+    ax2.set_yticks(np.arange(0,101,10))
     plt.tight_layout()
     # plt.show()
     plt.savefig('ResultsGraphs2.png') # change depending on model
@@ -335,12 +356,13 @@ if __name__ == "__main__":
 
     # Accuracy against specific attacks
     # generate_custom_attack_csv_files()
-    attack_accuracy_array, false_positives_array = generate_predictions_for_all_attacks(model_name)
+    attack_accuracy_array, false_positives_array, true_positives_array = generate_predictions_for_all_attacks(model_name)
     print(f"Accuracy against specific attacks: {attack_accuracy_array}")
     print(f"False positives for specific attacks: {false_positives_array}")
+    print(f"True positives for specific attacks: {true_positives_array}")
 
     # Visually display the accuracy against specific attacks
-    generate_prediction_graphs(attack_accuracy_array, false_positives_array)
+    generate_prediction_graphs(attack_accuracy_array, false_positives_array, true_positives_array)
     
     # Accuracy against live capture
     # demonstrate_model_performance()
